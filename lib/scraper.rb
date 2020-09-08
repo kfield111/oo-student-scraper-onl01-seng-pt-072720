a2e970de42afbe721cb91c6e4965ba67c10ed642
Kelin Field <kfield111@hotmail.com>
2020-09-08 23:25:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
     doc = Nokogiri::HTML(open(index_url))
     doc.css("div.student-card").each do |temp|
       student = {}
-      student = {name: temp.css("h4.student-name").text, location: temp.css("p.student-location").text, profile_url: temp.css("a").map {|link| link['href']}}
+      student = {name: temp.css("h4.student-name").text, location: temp.css("p.student-location").text, profile_url: temp.css("a")}
       scrapped_students << student
     end
     scrapped_students
