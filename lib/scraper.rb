e8a627018138338b0e5434efee42edf8c876a11c
Kelin Field <kfield111@hotmail.com>
2020-09-08 22:13:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,6 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(index_url)
-    doc.CSS("div.roster-cards-container")
   end
 
   def self.scrape_profile_page(profile_url)
