7aa3d8b12d9d92a4ee1bf9dc50a456da943f2cad
Kelin Field <kfield111@hotmail.com>
2020-09-08 22:15:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,10 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(index_url)
+
+    scraped_students = {}
+
+    doc.
   end
 
   def self.scrape_profile_page(profile_url)
