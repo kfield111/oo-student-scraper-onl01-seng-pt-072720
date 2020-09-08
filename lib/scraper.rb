90e1c6aaf479b4ea62fccadaa31142789133e151
Kelin Field <kfield111@hotmail.com>
2020-09-08 22:51:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,10 +8,17 @@ class Scraper
   def self.scrape_index_page(index_url)
 
     scrapped_students = []
+    student = {}
 
     doc = Nokogiri::HTML(open(index_url))
     doc.css("div.roster-cards-container").each do |temp|
       student = temp.css("div.card-text-container")
+      
+
+
+
+
+
       scrapped_students << student
     end
     scrapped_students
