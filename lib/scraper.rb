cb8344bd450f464a9ce0aa11eb80f20001d6b3f2
Kelin Field <kfield111@hotmail.com>
2020-09-09 02:30:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,6 +26,8 @@ class Scraper
     profile_page = Nokogiri::HTML(open(profile_url))
     social_media_hrefs = profile_page.css("div.social-icon-container a").map {|link| link['href']}
 
+    profile_page.css("div.profile-quote")
+
     profile_page.css("div.details-container").each do |temp|
       student_info = {twitter: social_media_hrefs[0], linkedin: social_media_hrefs[1], github: social_media_hrefs[2], blog: social_media_hrefs[3],
       profile_quote: temp.css('div.profile-quote' ).text, bio: temp.css("div.description-holder p").text}
