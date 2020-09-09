f5acc441b1d58ee12acaf26d8240be13ca219f6d
Kelin Field <kfield111@hotmail.com>
2020-09-09 03:37:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,6 +22,7 @@ class Student
   def add_student_attributes(attributes_hash)
     @bio = attributes_hash[:bio]
     @blog = attributes_hash[:blog]
+    @linkedin = attributes_hash[:linkden]
   end
 
   def self.all
