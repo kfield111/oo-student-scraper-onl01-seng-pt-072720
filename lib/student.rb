780bf0a01a6827ebe42ec851fcc937ad673d6b0b
Kelin Field <kfield111@hotmail.com>
2020-09-09 03:34:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-   students_array.map
+   students_array.each do |temp|
+     Student.new(temp)
+   end
   end
 
   def add_student_attributes(attributes_hash)
